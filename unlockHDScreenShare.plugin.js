93509cfa2f3e756f0bac46a4931ec1c47b9c21da
Aleksey Lapunov <lapunoff.alex@yandex.ru>
2025-11-29 12:41:42 +0700

    Patch for removing deprecated API functions
    These functions stopped working in recent BetterDiscord updates

--- unlockHDScreenShare.plugin.js
+++ unlockHDScreenShare.plugin.js
@@ -1,24 +1,40 @@
 /**
  * @name unlockHDScreenShare
- * @version 0.0.2
+ * @version 0.0.2p
  * @description unlock the ability to share your screen in 1080p
- * @author Ralkey#0516
- * @source https://github.com/RalkeyOfficial/discord-1080p-video-unlocker
+ * @author Ralkey#0516, AlekseyLapunov
+ * @source https://github.com/AlekseyLapunov/discord-1080p-video-unlocker-patch
  *   
  */
 
-
-module.exports = class unlockHDScreenShare{
-     
+module.exports = class unlockHDScreenShare {
+    
     constructor() {
-        this.userPremiumType = BdApi.findModuleByProps("getCurrentUser").getCurrentUser().premiumType;
+        this.userPremiumType = null;
+        this.UserStore = BdApi.Webpack.getModule(m => m.getCurrentUser);
     }
      
-    start() { 
-        BdApi.findModuleByProps("getCurrentUser").getCurrentUser().premiumType = 2;
+    start() {
+        if (!this.UserStore) {
+            BdApi.UI.showToast("Could not find User Store. Plugin cannot be loaded", { type: "error" });
+            return;
+        }
+
+        const currentUser = this.UserStore.getCurrentUser();
+        if (currentUser) {
+            this.userPremiumType = currentUser.premiumType;
+            currentUser.premiumType = 2;
+        } else {
+            BdApi.UI.showToast("Could not find Current User. Plugin cannot be loaded.", { type: "error" });
+        }
     }
     
     stop() {
-        BdApi.findModuleByProps("getCurrentUser").getCurrentUser().premiumType = this.userPremiumType;
+        if (this.UserStore && this.userPremiumType !== null) {
+            const currentUser = this.UserStore.getCurrentUser();
+            if (currentUser) {
+                currentUser.premiumType = this.userPremiumType;
+            }
+        }
     }
 }
